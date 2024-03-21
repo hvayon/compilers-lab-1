eba5d43bda287ddb37e574de4f89d44562c5420c
Губеева Наталья Олеговна <NOGubeeva@sberbank.ru>
2024-03-21 23:11:13 +0300

    2. According to the NCA, it builds an equivalent DCA

---
+++ src/main/java/ru/hvayon/compilers/graph/Pair.java
@@ -0,0 +1,34 @@
+package ru.hvayon.compilers.graph;
+
+import java.util.ArrayList;
+
+public class Pair {
+    ArrayList<Integer> resIntList;
+    ArrayList<Character> resCharList;
+
+    public Pair() {
+        resIntList = new ArrayList<>();
+        resCharList = new ArrayList<>();
+    }
+
+    public Pair(ArrayList<Integer> resIntList, ArrayList<Character> resCharList) {
+        this.resIntList = resIntList;
+        this.resCharList = resCharList;
+    }
+
+    public ArrayList<Integer> getResIntList() {
+        return resIntList;
+    }
+
+    public void setResIntList(ArrayList<Integer> resIntList) {
+        this.resIntList = resIntList;
+    }
+
+    public ArrayList<Character> getResCharList() {
+        return resCharList;
+    }
+
+    public void setResCharList(ArrayList<Character> resCharList) {
+        this.resCharList = resCharList;
+    }
+}
